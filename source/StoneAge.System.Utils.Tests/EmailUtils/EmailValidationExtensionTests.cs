2dd847c0f8df50e9602d6a28a38c09e9739ed11a
T-rav <tmfrisinger@gmail.com>
2018-11-09 11:06:26 +0200

    feat: added email validation extension

---
+++ source/StoneAge.System.Utils.Tests/EmailUtils/EmailValidationExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.EmailUtils;
+
+namespace StoneAge.System.Utils.Tests.EmailUtils
+{
+    [TestFixture]
+    public class EmailValidationExtensionTests
+    {
+        [Test]
+        public void Is_Valid_Email_WhenValidEmail_ShouldReturnTrue()
+        {
+            //---------------Arrange------------------
+            var input = "t@bizvoip.co.za";
+            //---------------Act----------------------
+            var actual = input.Is_Valid_Email();
+            //---------------Assert-------------------
+            actual.Should().BeTrue();
+        }
+
+        [Test]
+        public void Is_Valid_Email_WhenInvalidEmail_ShouldReturnFalse()
+        {
+            //---------------Arrange------------------
+            var input = "t@foo";
+            //---------------Act----------------------
+            var actual = input.Is_Valid_Email();
+            //---------------Assert-------------------
+            actual.Should().BeFalse();
+        }
+    }
+}
---
+++ source/StoneAge.System.Utils/EmailUtils/EmailValidationExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StoneAge.System.Utils.EmailUtils
+{
+    public static class EmailValidationExtension
+    {
+        public static bool Is_Valid_Email(this string input)
+        {
+            var emailValidationRegex = CreateValidEmailRegex();
+            return emailValidationRegex.IsMatch(input);
+        }
+
+        private static Regex CreateValidEmailRegex()
+        {
+            var validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                                    + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                                    + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(validEmailPattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
