90d82a2295364ef2afc73ee7108985438af77f13
T-rav <tmfrisinger@gmail.com>
2018-11-09 13:45:06 +0200

    chore : added object property hashing

---
+++ source/StoneAge.System.Utils.Tests/Hash/ObjectPropertyHashTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.Hash;
+
+namespace StoneAge.System.Utils.Tests.Hash
+{
+    [TestFixture]
+    public class ObjectPropertyHashTests
+    {
+        [Test]
+        public void Calculate_Hash_WhenAllProperties_ShouldReturnObjectHash()
+        {
+            //---------------Arrange------------------
+            var sut = new XTeamUser
+            {
+                PortaId = 999,
+                Login = "user1",
+                Password = "666",
+                Disabled = false,
+                Auth0Connection = "XTeam-Username"
+            };
+            //---------------Act----------------------
+            var actual = sut.Calculate_Hash();
+            //---------------Assert-------------------
+            var expected = "48da4949dcc46921a5b497d79ecc639763e3d9a972d9baaf19b057d2fbbce87f";
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+
+    internal class XTeamUser
+    {
+        public int PortaId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool Disabled { get; set; }
+        public string Auth0Connection { get; set; }
+    }
+}
---
+++ source/StoneAge.System.Utils/Hash/ObjectPropertyHash.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StoneAge.System.Utils.Hash
+{
+    public static class ObjectPropertyHash
+    {
+        private static readonly IEnumerable<string> AllProperties = null;
+
+        public static string Calculate_Hash<T>(this T instance)
+        {
+            var propertyString = Concat_Properites(instance, AllProperties);
+
+            return Calculate_Hash(propertyString);
+        }
+
+        private static string Calculate_Hash(string propertyString)
+        {
+            using (var sha256Hash = SHA256.Create())
+            {
+                var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(propertyString));
+
+                return Convert_Bytes_To_String(bytes);
+            }
+        }
+
+        private static string Convert_Bytes_To_String(byte[] bytes)
+        {
+            var builder = new StringBuilder();
+            foreach (var t in bytes)
+            {
+                builder.Append(t.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        private static string Concat_Properites<T>(T instance, IEnumerable<string> propertyNamesToMatch)
+        {
+            var props = List_Properites<T>();
+            var result = new StringBuilder();
+
+            foreach (var prop in props)
+            {
+                if (Property_In_List(propertyNamesToMatch, prop))
+                {
+                    var propValue = prop.GetValue(instance, null);
+                    result.Append(propValue);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static bool Property_In_List(IEnumerable<string> matchingProperites, PropertyInfo prop)
+        {
+            return Equals(matchingProperites, AllProperties) || matchingProperites.Contains(prop.Name);
+        }
+
+        private static List<PropertyInfo> List_Properites<T>()
+        {
+            var properties = typeof(T).GetProperties();
+            var orderedProperties = properties.OrderBy(x => x.Name);
+            var result = new List<PropertyInfo>(orderedProperties);
+            return result;
+        }
+    }
+}
