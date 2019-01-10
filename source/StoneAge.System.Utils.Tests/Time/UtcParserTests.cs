9e26e19f22572dd88d394e765931b820d7e78378
T-rav <t@bizvoip,co.za>
2019-01-10 14:42:49 -0700

    feat: added Parse_As_Utc_Then_Add_Current_Timezone_Offset

---
+++ source/StoneAge.System.Utils.Tests/Time/UtcParserTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.Time;
+
+namespace StoneAge.System.Utils.Tests.Time
+{
+    [TestFixture]
+    public class UtcParserTests
+    {
+        [Test]
+        public void Parse_As_Utc_Then_Add_Current_Timezone_Offset_ShouldReturnUtcTimeWithTimeZoneOffsetAdded()
+        {
+            //---------------Arrange-------------------
+            var input = "2018-04-01 04:00:00";
+            //---------------Act----------------------
+            var actual = input.Parse_As_Utc_Then_Add_Current_Timezone_Offset();
+            //---------------Assert-----------------------
+            var timeZoneOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).Hours * -1;
+            var hour = 4 + timeZoneOffset;
+            var expected = DateTime.Parse($"2018-04-01 {hour:00}:00:00");
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ source/StoneAge.System.Utils/Time/UtcParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StoneAge.System.Utils.Time
+{
+    public static class UtcParser
+    {
+        public static DateTime Parse_As_Utc_Then_Add_Current_Timezone_Offset(this string dateTimeString)
+        {
+            var utcOffsetHours = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).Hours * -1;
+            return DateTime.SpecifyKind(DateTime.Parse(dateTimeString), DateTimeKind.Utc).AddHours(utcOffsetHours);
+        }
+    }
+}
