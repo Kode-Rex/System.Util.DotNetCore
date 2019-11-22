a041ebb8198134fea5c237ca86fc44c545049103
T-rav <t@stoneagetechnologies.com>
2019-11-22 10:11:01 -0700

    chore: fixed up Parse_As_Utc_Then_Add_Current_Timezone_Offset to handle day light savings

--- source/StoneAge.System.Utils.Tests/Time/UtcUtilsTests.cs
+++ source/StoneAge.System.Utils.Tests/Time/UtcUtilsTests.cs
@@ -8,18 +8,40 @@ namespace StoneAge.System.Utils.Tests.Time
     [TestFixture]
     public class UtcUtilsTests
     {
-        [Test]
-        public void Parse_As_Utc_Then_Add_Current_Timezone_Offset_ShouldReturnUtcTimeWithTimeZoneOffsetAdded()
+        [TestFixture]
+        class Parse_As_Utc_Then_Add_Current_Timezone_Offset
         {
-            //---------------Arrange-------------------
-            var input = "2018-04-01 04:00:00";
-            //---------------Act----------------------
-            var actual = input.Parse_As_Utc_Then_Add_Current_Timezone_Offset();
-            //---------------Assert-----------------------
-            var timeZoneOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).Hours * -1;
-            var hour = 4 + timeZoneOffset;
-            var expected = DateTime.Parse($"2018-04-01 {hour:00}:00:00");
-            actual.Should().Be(expected);
+            [Test]
+            public void Given_Date_In_Daylight_Savings_ShouldReturnUtcTimeWithTimeZoneOffsetAdded()
+            {
+                //---------------Arrange-------------------
+                var input = "2018-04-01 04:00:00";
+                var parsedDateTime = DateTime.Parse(input);
+                //---------------Act----------------------
+                var actual = input.Parse_As_Utc_Then_Add_Current_Timezone_Offset();
+                //---------------Assert-----------------------
+                var timeZoneOffset = TimeZoneInfo.Local.GetUtcOffset(parsedDateTime).Hours * -1;
+                var hour = 4 + timeZoneOffset;
+                var expected = DateTime.Parse($"2018-04-01 {hour:00}:00:00");
+                actual.Should().Be(expected);
+                actual.Should().Be(expected);
+            }
+
+            [Test]
+            public void Given_Date_Outside_Daylight_Savings_ShouldReturnUtcTimeWithTimeZoneOffsetAdded()
+            {
+                //---------------Arrange-------------------
+                var input = "2018-12-01 04:00:00";
+                var parsedDateTime = DateTime.Parse(input);
+                //---------------Act----------------------
+                var actual = input.Parse_As_Utc_Then_Add_Current_Timezone_Offset();
+                //---------------Assert-----------------------
+                var timeZoneOffset = TimeZoneInfo.Local.GetUtcOffset(parsedDateTime).Hours * -1;
+                var hour = 4 + timeZoneOffset;
+                var expected = DateTime.Parse($"2018-12-01 {hour:00}:00:00");
+                actual.Should().Be(expected);
+                actual.Should().Be(expected);
+            }
         }
     }
 }
--- source/StoneAge.System.Utils/Time/UtcUtils.cs
+++ source/StoneAge.System.Utils/Time/UtcUtils.cs
@@ -6,8 +6,16 @@ namespace StoneAge.System.Utils.Time
     {
         public static DateTime Parse_As_Utc_Then_Add_Current_Timezone_Offset(this string dateTimeString)
         {
+            var dateTime = DateTime.Parse(dateTimeString);
             var utcOffsetHours = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).Hours * -1;
-            return DateTime.SpecifyKind(DateTime.Parse(dateTimeString), DateTimeKind.Utc).AddHours(utcOffsetHours);
+            
+            if(TimeZoneInfo.Local.IsDaylightSavingTime(dateTime))
+            {
+                utcOffsetHours -= 1;
+            }
+
+            var utcTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+            return utcTime.AddHours(utcOffsetHours);
         }
     }
 }
