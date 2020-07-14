3b5c1e8eeb19c4e26de198bfe2597ac484e1b651
Trav <t@koderex.co.za>
2020-07-14 10:02:30 -0600

    feat: added Split_List to chunk up a big list into parts

---
+++ source/StoneAge.System.Utils.Tests/List/ListExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.Json;
+using System.Linq;
+
+namespace StoneAge.System.Utils.Tests.Json
+{
+    [TestFixture]
+    public class ListExtensionsTests
+    {
+        [Test]
+        public void Split_List_When_100_Items_Split_By_20_Expect_5_Chunks()
+        {
+            //---------------Set up test pack-------------------
+            var input = Enumerable.Range(0, 100).ToList();
+            //---------------Execute Test ----------------------
+            var result = input.Split_List(20);
+            //---------------Test Result -----------------------
+            var expected = 5;
+            result.Count().Should().Be(expected);
+        }
+
+        [Test]
+        public void Split_List_When_100_Items_Split_By_Default_10_Expect_10_Chunks()
+        {
+            //---------------Set up test pack-------------------
+            var input = Enumerable.Range(0, 100).ToList();
+            //---------------Execute Test ----------------------
+            var result = input.Split_List();
+            //---------------Test Result -----------------------
+            var expected = 10;
+            result.Count().Should().Be(expected);
+        }
+    }
+}
---
+++ source/StoneAge.System.Utils/List/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoneAge.System.Utils
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<List<T>> Split_List<T>(this List<T> data, int chunkSize = 10)
+        {
+            for (int i = 0; i < data.Count; i += chunkSize)
+            {
+                yield return data.GetRange(i, Math.Min(chunkSize, data.Count - i));
+            }
+        }
+    }
+}
