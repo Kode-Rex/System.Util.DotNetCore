8d8cac78a2a56693e6a15c2c1bd923d6c69f33d8
Trav <t@koderex.co.za>
2020-09-09 19:24:38 -0600

    feat: added ExtractField for things like 'DataRecordInternal'

---
+++ source/StoneAge.System.Utils.Tests/Extract/ObjectValueExtractorTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.Extract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoneAge.System.Utils.Tests.Extract
+{
+    [TestFixture]
+    public class ObjectValueExtractorTests
+    {
+        // todo : test default
+        [Test]
+        public void When_Primative_Exist_Expect_Value()
+        {
+            // arrange
+            var testObject = new TestObject(5, new List<string> { "abc", "def" });
+            // act
+            var actual = testObject.ExtractField<int>("_value1");
+            // assert
+            var expected = 5;
+            actual.Should().Be(expected);
+        }
+
+        [Test]
+        public void When_Primative_Does_Not_Exist_Expect_Default_Value()
+        {
+            // arrange
+            var testObject = new TestObject(5, new List<string> { "abc", "def" });
+            // act
+            var actual = testObject.ExtractField<int>("_value1992349");
+            // assert
+            var expected = 0;
+            actual.Should().Be(expected);
+        }
+
+        [Test]
+        public void When_Object_Exist_Expect_Value()
+        {
+            // arrange
+            var testObject = new TestObject(5, new List<string> { "abc", "def" });
+            // act
+            var actual = testObject.ExtractField<List<string>>("_value2");
+            // assert
+            var expected = new List<string> { "abc", "def" };
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void When_OBject_Does_Not_Exist_Expect_Null()
+        {
+            // arrange
+            var testObject = new TestObject(5, new List<string> { "abc", "def" });
+            // act
+            var actual = testObject.ExtractField<List<string>>("_value1992349");
+            // assert
+            actual.Should().BeNull();
+        }
+    }
+
+    public class TestObject
+    {
+        private int _value1;
+        private List<string> _value2;
+
+        public TestObject(int value1, List<string> value2)
+        {
+            _value1 = value1;
+            _value2 = value2;
+        }
+    }
+}
---
+++ source/StoneAge.System.Utils/Extract/ObjectValueExtractor.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace StoneAge.System.Utils.Extract
+{
+    public static class ObjectValueExtractor
+    {
+        public static T ExtractField<T>(this object instance1, string fieldName)
+        {
+            var fields = instance1.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance).ToList();
+            var field = fields.FirstOrDefault(x=>x.Name == fieldName);
+
+            if(field == null)
+            {
+                return default(T);
+            }
+
+            var value = field.GetValue(instance1);
+
+            return (T)value;
+        }
+    }
+}
