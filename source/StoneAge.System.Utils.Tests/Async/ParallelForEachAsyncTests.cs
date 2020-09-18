3b82707d92a987eacd2a10fa8c53e809198659b4
Trav <t@koderex.co.za>
2020-09-18 11:10:31 -0600

    feat: added ForEachAsync

---
+++ source/StoneAge.System.Utils.Tests/Async/ParallelForEachAsyncTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StoneAge.System.Utils.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneAge.System.Utils.Tests.Async
+{
+    [TestFixture]
+    public class ParallelForEachAsyncTests
+    {
+        [Test]
+        public async Task test()
+        {
+            // arrange
+            var result = new List<int>();
+            var input = Enumerable.Range(1, 20);
+            // act
+            await input.ForEachAsync(new Func<int, Task>(async x =>
+            {
+                result.Add(x + 1);
+            }), 2);
+            // assert
+            result.Count().Should().Be(20);
+        }
+    }
+}
---
+++ source/StoneAge.System.Utils/Async/ParallelForEachAsync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StoneAge.System.Utils.Async
+{
+    public static class ParallelForEachAsync
+    {
+        public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> action, int maxParallelCount = 4)
+        {
+            using (SemaphoreSlim completeSemphoreSlim = new SemaphoreSlim(1))
+            using (SemaphoreSlim taskCountLimitsemaphoreSlim = new SemaphoreSlim(maxParallelCount))
+            {
+                await completeSemphoreSlim.WaitAsync();
+                int runningtaskCount = source.Count();
+
+                foreach (var item in source)
+                {
+                    await taskCountLimitsemaphoreSlim.WaitAsync();
+
+                    Task.Run(async () =>
+                    {
+                        try
+                        {
+                            await action(item).ContinueWith(task =>
+                            {
+                                Interlocked.Decrement(ref runningtaskCount);
+                                if (runningtaskCount == 0)
+                                {
+                                    completeSemphoreSlim.Release();
+                                }
+                            });
+                        }
+                        finally
+                        {
+                            taskCountLimitsemaphoreSlim.Release();
+                        }
+                    }).GetHashCode();
+                }
+
+                await completeSemphoreSlim.WaitAsync();
+            }
+        }
+    }
+}
