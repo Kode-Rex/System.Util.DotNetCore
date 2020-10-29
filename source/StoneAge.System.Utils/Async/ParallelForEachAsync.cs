6f77c7eef0b1751ce8c49d9b4b8333f430fc5005
Trav <t@koderex.co.za>
2020-10-29 16:27:51 -0600

    bug: revert error catching since it causes single thread execution

--- source/StoneAge.System.Utils/Async/ParallelForEachAsync.cs
+++ source/StoneAge.System.Utils/Async/ParallelForEachAsync.cs
@@ -27,7 +27,7 @@ namespace StoneAge.System.Utils.Async
                 {
                     await taskCountLimitsemaphoreSlim.WaitAsync();
 
-                    await Task.Run(async () =>
+                    Task.Run(async () =>
                     {
                         try
                         {
@@ -38,27 +38,69 @@ namespace StoneAge.System.Utils.Async
                                 {
                                     completeSemphoreSlim.Release();
                                 }
-
-                                if (task.Exception != null)
-                                {
-                                    throw task.Exception.InnerException;
-                                }
-
                             });
-                        }
-                        catch (Exception e)
-                        {
-                            throw;
                         }
                         finally
                         {
                             taskCountLimitsemaphoreSlim.Release();
                         }
-                    });
+                    }).GetHashCode();
                 }
 
                 await completeSemphoreSlim.WaitAsync();
             }
         }
+
+
+        //public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> action, int maxParallelCount = 4)
+        //{
+        //    if (source.Count() == 0)
+        //    {
+        //        return;
+        //    }
+
+        //    using (SemaphoreSlim completeSemphoreSlim = new SemaphoreSlim(1))
+        //    using (SemaphoreSlim taskCountLimitsemaphoreSlim = new SemaphoreSlim(maxParallelCount))
+        //    {
+        //        await completeSemphoreSlim.WaitAsync();
+        //        int runningtaskCount = source.Count();
+
+        //        foreach (var item in source)
+        //        {
+        //            await taskCountLimitsemaphoreSlim.WaitAsync();
+
+        //            await Task.Run(async () =>
+        //            {
+        //                try
+        //                {
+        //                    await action(item).ContinueWith(task =>
+        //                    {
+        //                        Interlocked.Decrement(ref runningtaskCount);
+        //                        if (runningtaskCount == 0)
+        //                        {
+        //                            completeSemphoreSlim.Release();
+        //                        }
+
+        //                        if (task.Exception != null)
+        //                        {
+        //                            throw task.Exception.InnerException;
+        //                        }
+
+        //                    });
+        //                }
+        //                catch (Exception e)
+        //                {
+        //                    throw;
+        //                }
+        //                finally
+        //                {
+        //                    taskCountLimitsemaphoreSlim.Release();
+        //                }
+        //            });
+        //        }
+
+        //        await completeSemphoreSlim.WaitAsync();
+        //    }
+        //}
     }
 }
